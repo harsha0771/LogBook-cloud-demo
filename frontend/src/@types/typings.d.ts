5fa878abec80c35b3dc6e4ac0406427e33f56574
harsha0771 <harshagihan00@gmail.com>
2025-07-28 20:37:24 +0530

    h

--- frontend/src/@types/typings.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'color-thief-browser' {
-    export default class ColorThief {
-      getColor(sourceImage: HTMLImageElement | HTMLCanvasElement, quality?: number): number[];
-      getPalette(sourceImage: HTMLImageElement | HTMLCanvasElement, colorCount?: number, quality?: number): number[][];
-    }
-  }
-  
--- frontend/src/custom.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'jwt-decode';
-declare module 'jspdf';
-declare module 'html2canvas';
-declare module 'qrcode';
-declare module 'pdfmake/build/pdfmake';
-declare module 'pdfmake/build/vfs_fonts';
